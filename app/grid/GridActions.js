55ee072c5f9dd513f0c9e7f870649684226d2444
George Buhanov <georgebuhanov@Georges-Mac-mini.local>
2015-06-15 00:43:51 +0500

    fix

--- app/grid/GridActions.js
+++ app/grid/GridActions.js
@@ -1,6 +1,19 @@
 var constants = require('./constants');
 var dispatch = require('../shared/helpers/dispatch');
 
+// _(revenue.concat(costs)).groupBy(function (p) {
+// 	return p.date
+// }).map(function (value, key) {
+// 	return {
+// 		date: key,
+// 		revenue: value[0].value,
+// 		costs: value[1] ? value[1].value : '-'
+// 	};
+// }).value();
+
+//получился пропуск значения
+// _.merge(revenue, costs, function(r, c){ return  {date: r.date, revenue: r.value, cost: c.value }  });
+
 module.exports = {
     load: function () {
         var gridData = {
--- app/grid/components/Grid.js
+++ app/grid/components/Grid.js
@@ -5,18 +5,6 @@ var GridStore = require('../GridStore');
 var GridRow = require('./GridRow');
 var GridActions = require('../GridActions');
 
-// _(revenue.concat(costs)).groupBy(function (p) {
-// 	return p.date
-// }).map(function (value, key) {
-// 	return {
-// 		date: key,
-// 		revenue: value[0].value,
-// 		costs: value[1] ? value[1].value : '-'
-// 	};
-// }).value();
-
-//получился пропуск значения
-// _.merge(revenue, costs, function(r, c){ return  {date: r.date, revenue: r.value, cost: c.value }  });
 module.exports = React.createClass({
 	mixins: [storeMixin(GridStore)],
 
